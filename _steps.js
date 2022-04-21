e194306caa9abc7f50a78d8d2e90b346791e1703
devnoman01 <devnoman01@gmail.com>
2022-04-21 20:03:08 +0600

    simple react node express use

---
+++ _steps.js
@@ -0,0 +1,14 @@
+/**
+ * ----- basic -----
+ * 1. create a folder
+ * 2. open command line to that folder
+ * 3. >> npm init
+ * ----- for express/node server -----
+ * 1. >> npm install express
+ * 2. create index.js file
+ * 3. create app variable
+ * 4. declare port
+ * 5. set app.get('/')
+ * 6. listen to port
+ * 7. >> node index.js
+ */
---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+var cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Listening to request and checking");
+});
+
+const users = [
+  { id: 1, name: "Katrina", email: "katrina@gmail.com", phone: "0123456789" },
+  { id: 2, name: "Priyanka", email: "priyanka@gmail.com", phone: "0123456789" },
+  { id: 3, name: "jaquelin", email: "jaquelin@gmail.com", phone: "0123456789" },
+  { id: 4, name: "Katrina", email: "katrina@gmail.com", phone: "0123456789" },
+  { id: 5, name: "Rakul", email: "rakul@gmail.com", phone: "0123456789" },
+  { id: 6, name: "Sunny", email: "sunny@gmail.com", phone: "0123456789" },
+];
+
+app.get("/users", (req, res) => {
+  console.log("query", req.query);
+  if (req.query.name) {
+    const search = req.query.name.toLowerCase();
+    const matched = users.filter((user) =>
+      user.name.toLowerCase().includes(search)
+    );
+    res.send(matched);
+  } else {
+    res.send(users);
+  }
+});
+app.get("/users/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const user = users.find((u) => u.id === id);
+  res.send(user);
+});
+
+app.post("/user", (req, res) => {
+  console.log("request", req.body);
+  const user = req.body;
+  user.id = users.length + 1;
+  users.push(user);
+  res.send(user);
+});
+
+app.get("/fruits", (req, res) => {
+  res.send(["lemon", "orange", "mango"]);
+});
+
+app.listen(port, () => {
+  console.log("Listening to port");
+});
